2ad427415e23aae66db55f72cffd89871f7e73be
NajmaddinGozalov <gozelovneco@gmail.com>
2022-03-18 11:09:07 +0400

    comm

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-
-$(document).on('click', '.tablePlusIcon', function (ev) {
-    console.log('zxz');
-    $(this).closest('.tableCon').find('.addInput').toggleClass('d-none');
+// plus iconu click
+$(document).on('click', '.tablePlusIcon', function () {
+    
+    $(this).closest('.tableCon').find('.addInput').last().toggleClass('d-none');
     $(this).toggleClass('tablePlusIconclick')
+})
+// add descriptiom
+$(document).on('change', '.tableEndInp', function () {
+    var text = $(this).val();
+    var table = $(this).closest('.tableCon').find('table tbody').last();
+
+    table.append(`
+     <tr>
+      <td>
+        <div class="titleTbody">
+         <div class="titleTbodyCheck dFlex">
+           <input type="checkbox">
+         </div>
+         <div class="descSortArrow">
+            <i class="far fa-expand-arrows"></i>
+         </div>
+         <div class="titleTbodyText">- ${text}</div>
+           </td>
+     </tr>
+    `);
+    $(this).val('');
 })
