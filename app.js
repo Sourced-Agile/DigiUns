cd046a02535d84efb123c5e8319ee219ab58a9cf
NajmaddinGozalov <gozelovneco@gmail.com>
2022-03-23 10:25:49 +0400

    asasd

--- app.js
+++ app.js
@@ -68,67 +68,139 @@ const miniStoryCard = {
   sortableDec: function () {   
     $(".tableTitle tbody").sortable();
 
-  },
-  addInput:function (el) {
-     var ed = $('.tableTitle_sira').last().text();
-     var num = parseInt(ed) + 1;
-     var nam = $(el).val();     
-    $('.cardBody').append(`
-                       <div class="mb-2 mr-2 tableCon">
-                        <table class="tableTitle">
-                            <thead>
-                                <tr>
-                                    <th style="width:22px w-100">
-                                        <div class="d-flex" style="position:relative;">
-                                            <div class="tableTitle_sira title dFlex text-light">
-                                                ${num}
+  },  
+  addIpsum: function (el, val) {
+    var dd = $('.ipsumBodyContainer').last().find('.ipsumBodyForOrder').text();
+    var a=parseInt(dd)+1
+  console.log(dd);
+    var com = (`<div class="ipsumBodyContainer">
+                                <div class="mb-2 mr-2 mt-1 w-100 tableCon">
+                                    <table class="tableTitle">
+                                <thead>
+                                    <tr>
+                                        <th class="w-100">
+                                            
+                                            <div class="ipsumBodyTitleCh">
+                                                 <div class="ipsumBodyTitleChDrag" style="cursor: pointer;">
+                                                    <i class="far fa-ellipsis-v"></i>
+                                                    <i class="far fa-ellipsis-v"></i>
+                                                  </div>
+                                                <div class="ipsumBodyTitleChDrag title ipsumBodyForOrder">
+                                                    ${a}
+                                                </div>
+                                                <div class="ipsumBodyTitleChhead">                                                                                                     
+
+                                                    <div class="impsumTitleYazisi title">${val}</div>
+                                                     <div class="ipsumCHTitleRight">
+                                                       <i class="fal fa-ellipsis-v text-light"></i>
+                                                       <input class="" type="checkbox" style="width: 10px;">
+                                                    </div> 
+                                                   
+                                                    </div>
+                                                <div class="">                                                    
+                                                    <button class="tablePlusIcon">
+                                                        <i class="far fa-plus"></i>
+                                                    </button>
+
+                                                </div>
+
                                             </div>
-                                            <div class="tableTitle_title dFlex">
-                                                <a href="#" class="text-light"><i
-                                                        class="fal fa-ellipsis-v text-light buttontable"></i></a>
+                                        </th>
 
-                                                <div class="text-light title">${nam}</div>
-                                                <input type="checkbox" style="width: 10px;">
-                                            </div>
-                                            <div class="dFlex">
-                                                <button class="tablePlusIcon">
-                                                    <i class="far fa-plus"></i>
-                                                </button>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                </tbody>
+                                    </table>
+                                    <div class="bgF7F7F7 d-flex justify-content-end ">
+                                    <input class="tableEndInp addInput d-none" placeholder="Title" type="text">
+                                    </div>
+                                </div>
+                                
 
-                                            </div>
-
-                                        </div>
-                                    </th>
-
-                                </tr>
-                            </thead>
-                            <tbody>                                
-                            </tbody>
-                            <div class="bgF7F7F7 d-flex justify-content-end ">
-                            <input class="tableEndInp addInput d-none" type="text">
-                        </div>
-                        </table>
-                        <div class="bgF7F7F7 d-flex justify-content-end ">
-                            <input class="tableEndInp addInput d-none" placeholder="Title" type="text">
-                        </div>
-                    </div>
-  `);
-    $(el).val('');
+                            </div>
+                    `);
+    $('.ipsumContaineriAddUcun').append(com);
   }
-
+   
 }
-miniStoryCard.init();
+
  
 
 // plus iconu click
 $(document).on('click', '.tablePlusIcon', function () {
   miniStoryCard.showInputForDesc(this)
 })
+// ipsum plus click
+$(document).on('click', '#plusAddIpsum', function () {
+  $(this).find('i').toggleClass('fa-times fa-plus')
+  $('#addIpsumInputt').toggleClass('d-none')
+})
+//ipsum add
+$(document).on('change', '#addIpsumInputt', function () {
+  v = $(this).val(); 
+  miniStoryCard.addIpsum(this,v)
+})
 // add descriptiom change
 $(document).on('change', '.tableEndInp', function () {
   miniStoryCard.addNewDesc(this)
 })
+//menu bar
+$(document).on('click', '#ipsumCartmenuIcon', function () {
+  $(this).toggleClass('ipsumMwnuIconactive ipsumMwnuIcon');
+  $(this).find('i').toggleClass('fa-angle-double-up fa-angle-double-down')
+  $('.ipsumContaineriAddUcun').toggleClass('d-none')
 
-$(document).on('change', '#addInput', function () {
- miniStoryCard.addInput(this)
+
+  
+  	  var $span = $(this),
+  	    $parentTr = $span.closest("div");
+
+  	  //remove
+  	  if ($span.hasClass("fa-rotate-90")) {
+  	    $span.removeClass("fa-rotate-90");
+  	    $parentTr
+
+  	      .next()
+  	      .children("td")
+  	      .animate({
+  	        padding: 0
+  	      })
+  	      .wrapInner("<div/>")
+  	      .children()
+  	      .slideUp(function () {
+  	        var $tr = $(this).closest("div");
+
+  	        $tr.remove();
+  	        $parentTr.removeClass("tr-selected");
+  	      });
+
+  	    return;
+  	  }
+
+  	  //add
+  	  $span.addClass("fa-rotate-90");
+  	  if ($parentTr.next().hasClass("tr-detail")) return;
+  	  $parentTr.addClass("tr-selected");
+
+  	  $("#tr-detail")
+  	    .clone()
+  	    .removeClass("hidden")
+  	    .insertAfter($parentTr)
+  	    .children("td")
+  	    .animate()
+  	    .wrapInner("<div style='display:none'/>")
+  	    .children()
+  	    .slideDown();
+  	});
+
+
+
+$(document).on('click', '.cardHeaderPlus', function () {
+  $(this).toggleClass('cardHeaderPlusToggle');
+  $(this).find('i').toggleClass('fa-times fa-plus');
+  $('#addInput').toggleClass('headAddNewInp width0');
+  
 })
+
