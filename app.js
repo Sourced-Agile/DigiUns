bb91db1f91758bbd78dd1d19599c265cd6d5c402
NajmaddinGozalov <gozelovneco@gmail.com>
2022-03-29 14:08:00 +0400

    ease

--- app.js
+++ app.js
@@ -1,169 +1,119 @@
-const miniStoryCard = {
-  init: function (div, backlogId) {   
-    $(div).html(this.genBlock());
-    this.load.init(backlogId);
-    this.sortableDec();
-    
-  },
-  load:{
-        init: function (backlogId) {
-          console.log('init')
-        }
-  },
-  update:{
-      
-  },
-  insert:{
-      
-  },
-  delete:{
-      
-  },
-  genBlock:function (params) {
-        return `<div class="cartContainer">
-                   ${this.genLeftConatiner()}
-                   ${this.genRightConatiner()}
-                 </div>`
-  },
-  genLeftConatiner:function (url) {
-    return `<div class="photoContainer">
-                <img src="${url?url:"image"}" alt="">
-            </div>`
-  },
-  genRightConatiner: function (title) {
-    return `<div class="cartContainer">
-                <div class="cardheader">
-                    <div class="cardTitle">${title?title:"Story Card"}</div>
-                </div>
-                <div class="cardBody">
-                   
-                </div>
-            </div>`
-  },
-  showInputForDesc: function (el) {
-    $(el).closest('.tableCon').find('.addInput').last().toggleClass('d-none');
-    $(el).toggleClass('tablePlusIconclick')
-  },
-  addNewDesc: function (el) {
-    console.log('el')
-    var text = $(el).val();
-    var table = $(el).closest('.tableCon').find('table tbody').last();
+const SqlGeneratorClass = {
+    loader: function () {      
+        $('body').html(this.genBlock());
+         $('.selectpicker').selectpicker('refresh')
+    },
+    genBlock: function (params) {
+        return `
+                <div class="containerDbb"> 
+                   ${this.genSection1()}
+                   ${this.genSection2()}
+                </div> 
+                `
+    },
+    genSection1: function () {
+        return `
+                    <div class="section_box1Dbb">
+                    <!-- head -->
+                    <div class="inputContainerDbb">
+                        <div class="cs-input-boxDbb">
+                            <span>Database Name</span>
+                            <select class="selectpicker select" multiple id="" data-live-search="true" data-size="6"
+                                data-show-subtext="true">
+                        
+                            </select>
+                        </div>
+                        <div class="cs-input-boxDbb">
+                            <span>Table Name</span>
+                            <select class="selectpicker select" multiple id="" data-live-search="true" data-size="6"
+                                data-show-subtext="true">
 
-    table.append(`
-     <tr>
-      <td>
-        <div class="titleTbody">
-         <div class="titleTbodyCheck dFlex">
-           <input type="checkbox">
-         </div>
-         <div class="descSortArrow">
-            <i class="far fa-expand-arrows"></i>
-         </div>
-         <div class="titleTbodyText">- ${text}</div>
-           </td>
-     </tr>
-    `);
-    $(el).val('');
-  },
-  sortableDec: function () {   
-    $(".tableTitle tbody").sortable();
+                            </select>
+                        </div>
+                        <div class="cs-input-boxDbb">
+                            <span>Field Name</span>
+                            <select class="selectpicker select" multiple id="" data-live-search="true" data-size="6"
+                                data-show-subtext="true">
 
-  },  
-  addIpsum: function (el, val) {
-    var dd = $('.ipsumBodyContainer').last().find('.ipsumBodyForOrder').text();
-    var a=parseInt(dd)+1
-  console.log(dd);
-    var com = (`<div class="ipsumBodyContainer">
-                                <div class="mb-2 mr-2 mt-1 w-100 tableCon">
-                                    <table class="tableTitle">
-                                <thead>
-                                    <tr>
-                                        <th class="w-100">
-                                            
-                                            <div class="ipsumBodyTitleCh">
-                                                 <div class="ipsumBodyTitleChDrag" style="cursor: pointer;">
-                                                    <i class="far fa-ellipsis-v"></i>
-                                                    <i class="far fa-ellipsis-v"></i>
-                                                  </div>
-                                                <div class="ipsumBodyTitleChDrag title ipsumBodyForOrder">
-                                                    ${a}
-                                                </div>
-                                                <div class="ipsumBodyTitleChhead">                                                                                                     
+                            </select>
+                        </div>
+                        <div class="cs-input-boxDbb">
+                            <button class="headBtnDbb"><i class="fas fa-plus"></i></button>
+                        </div>
+                    </div>
+                    <!-- body -->
+                    <div class="tableBodyDb">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Database</th>
+                                    <th scope="col">Table</th>
+                                    <th scope="col">Field</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>                   
+                            
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
+        `
+    },
+    genSection2: function () {
+        return `
+             <div class="section_box2Dbb">
+                    <!-- head -->
+                    <div class="inputContainerDbb">
+                        <div class="cs-input-boxDbb">
+                            <span>Database Name</span>
+                            <select class="selectpicker select" multiple id="" data-live-search="true" data-size="6"
+                                data-show-subtext="true">
+                        
+                            </select>
+                        </div>
+                        <div class="cs-input-boxDbb">
+                            <span>Table Name</span>
+                            <select class="selectpicker select" multiple id="" data-live-search="true" data-size="6"
+                                data-show-subtext="true">
 
-                                                    <div class="impsumTitleYazisi title">${val}</div>
-                                                     <div class="ipsumCHTitleRight">
-                                                       <i class="fal fa-ellipsis-v text-light"></i>
-                                                       <input class="" type="checkbox" style="width: 10px;">
-                                                    </div> 
-                                                   
-                                                    </div>
-                                                <div class="">                                                    
-                                                    <button class="tablePlusIcon">
-                                                        <i class="far fa-plus"></i>
-                                                    </button>
+                            </select>
+                        </div>            
+                        <div class="cs-input-boxDbb">
+                            <button class="headBtnDbb"><i class="fas fa-plus"></i></button>
+                        </div>
+                    </div>
+                    <!-- body -->
+                    <div class="tableBodyDb">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Database</th>
+                                    <th scope="col">Table</th>                      
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>                  
+                            
+                            </tbody>
+                        </table>
+                    </div>
 
-                                                </div>
+        </div>
 
-                                            </div>
-                                        </th>
+        `
+    }
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                </tbody>
-                                    </table>
-                                    <div class="bgF7F7F7 d-flex justify-content-end ">
-                                    <input class="tableEndInp addInput d-none" placeholder="Title" type="text">
-                                    </div>
-                                </div>
-                                
-
-                            </div>
-                    `);
-    $('.ipsumContaineriAddUcun').append(com);
-  }
-   
 }
 
- 
 
-// plus iconu click
-$(document).on('click', '.tablePlusIcon', function () {
-  miniStoryCard.showInputForDesc(this)
-})
-// ipsum plus click
-$(document).on('click', '#plusAddIpsum', function () {
-  $(this).find('i').toggleClass('fa-times fa-plus')
-  $('#addIpsumInputt').toggleClass('d-none')
-})
-//ipsum add
-$(document).on('change', '#addIpsumInputt', function () {
-  v = $(this).val(); 
-  miniStoryCard.addIpsum(this,v)
-})
-// add descriptiom change
-$(document).on('change', '.tableEndInp', function () {
-  miniStoryCard.addNewDesc(this)
-})
-//menu bar
-$(document).on('click', '#ipsumCartmenuIcon', function () {
-  $(this).toggleClass('ipsumMwnuIconactive ipsumMwnuIcon');
-  $(this).find('i').toggleClass('fa-angle-double-up fa-angle-double-down')
-  $('.ipsumContaineriAddUcun').toggleClass('d-none')  
-  	  
-  	});
+// $(document).ready(function () {
+//     SqlGeneratorClass.loader();
+// })
 
 
+    
 
-$(document).on('click', '.cardHeaderPlus', function () {
-  $(this).toggleClass('cardHeaderPlusToggle');
-  $(this).find('i').toggleClass('fa-times fa-plus');
-  $('#addInput').toggleClass('headAddNewInp width0');
   
-})
-
-$(document).on('click', '.btnMEnuAcanselectUcun', function (event) {
-    event.stopPropagation();
-})
-
